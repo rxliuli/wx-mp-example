03d1c40608c507c03421b0adbad3727eaa86f4f7
rxliuli <rxliuli@gmail.com>
2018-09-11 19:04:04 +0800

    Initial commit

---
+++ src/main/java/com/rxliuli/example/wxmpexample/WxMpExampleApplication.java
@@ -0,0 +1,12 @@
+package com.rxliuli.example.wxmpexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxMpExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxMpExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rxliuli/example/wxmpexample/WxMpExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rxliuli.example.wxmpexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WxMpExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
